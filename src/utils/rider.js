62e64ed0f85768509adefa83682747ba659a76ca
bennovandorst <mail@ashlyn.nl>
2025-05-20 23:26:52 +0200

    Ignore Rider launch if not on windows

--- src/utils/rider.js
+++ src/utils/rider.js
@@ -1,11 +1,16 @@
 import os from 'os';
 import { promisify } from 'util';
 import { exec } from 'child_process';
-import { logSuccess, logError } from './logger.js';
+import {logSuccess, logError, logInfo} from './logger.js';
 
 const execAsync = promisify(exec);
 
 export async function launchRider() {
+  if (process.platform !== 'win32') {
+    logInfo('Skipping Rider launch: not on Windows');
+    return;
+  }
+
   const username = os.userInfo().username;
   const riderPath = `C:\\Users\\${username}\\AppData\\Local\\rider\\Rider.exe`;
 
